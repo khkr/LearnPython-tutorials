8c03dbee20100b9a878a8f497bc3f5c4ac6b506a
khkr <khkrlife@gmail.com>
2017-09-30 12:00:45 +0530

    Python learning

---
+++ ex7.py
@@ -0,0 +1,38 @@
+print("Mary had a little lamb.")
+
+# I need to pay special attention here
+print("Its fleece was white as {}.".format('snow'),"I know right" )
+
+print("And everywhere that Mary went.")
+
+#Important
+print("." * 10)
+
+end1 = "C"
+
+end2 = "h"
+
+end3 = "e"
+
+end4 = "e"
+
+end5 = "s"
+
+end6 = "e"
+
+end7 = "B"
+
+end8 = "u"
+
+end9 = "r"
+
+end10 = "g"
+
+end11 = "e"
+
+end12 = "r"
+
+#Understand the following line
+print(end1 + end2 + end3+ end4 + end5 + end6, end = ' ')
+
+print(end7 + end8 + end9 + end10 + end11 + end12)
