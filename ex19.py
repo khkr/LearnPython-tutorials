d9c5cfd8857ce2786b5846acd8e58aecec655582
khkr <khkrlife@gmail.com>
2017-10-02 16:36:46 +0530

    It works

--- ex19.py
+++ ex19.py
@@ -1,20 +1,24 @@
-#Checking to see how we can use direct values and variables 
+#We are going to see local and global variables 
+
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-	print("You have %d cheeses" %cheese_count)
-	print("You have %d boxes of crackers" %boxes_of_crackers)
+	print(f"You have {cheese_count} cheeses!")
+	print(f"You have {boxes_of_crackers} boxes_of_crackers!")
+	print("Man that's enough for a party!")
+	print("Get a blanket.\n")
+	
 
-#Direct Values here
-print("We can give the function argument values directly:")
-cheese_and_crackers(20,40)
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20,30)
 
-print("Or we can even use variables")
+print("Or we can use variables from our script:")
 
-#Variables here
+#These are local variables
 amount_of_cheese = 10
 amount_of_crackers = 50
-
 cheese_and_crackers(amount_of_cheese,amount_of_crackers)
 
-#We can also add values and variables too
-print("We can combine the variables and do math too")
-cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+print("we can even do math inside too:")
+cheese_and_crackers(10+20, 5+6)
+print("And we can even combine the two , variables and math:")
+cheese_and_crackers(amount_of_cheese+100,amount_of_crackers+1000)
