417868a58bd5ecd814a1603d0628215702c7175d
khkr <khkrlife@gmail.com>
2017-10-02 17:07:48 +0530

    21 days to go

--- ex19.py
+++ ex19.py
@@ -8,6 +8,7 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
 	
 
 print("We can just give the function numbers directly:")
+#Here we are passing just numbers
 cheese_and_crackers(20,30)
 
 print("Or we can use variables from our script:")
@@ -19,6 +20,7 @@ cheese_and_crackers(amount_of_cheese,amount_of_crackers)
 
 
 print("we can even do math inside too:")
+#Turns out even math can be added
 cheese_and_crackers(10+20, 5+6)
 print("And we can even combine the two , variables and math:")
 cheese_and_crackers(amount_of_cheese+100,amount_of_crackers+1000)
--- test.py
+++ test.py
@@ -3,10 +3,11 @@ from sys import argv
 script, file_name = argv
 #We are opening a file here and if it doesn't exist it'll be created
 #When we use 'w' it'll wipe the entire file and we have to start fresh
-f = open(file_name)
+f = open(file_name,'w+')
 
-print(f.read())
-
+for i in range(10):
+    f.write(f"This is line {i+1}\n")
+f.close()
 
 
 
